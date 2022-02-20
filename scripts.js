a1da3adee9f44dc8a8c7169f7d9484f3faee159b
Brujitari <natimontesg2@mail.com>
2022-02-20 21:47:36 +0100

    feat: console displays domain

--- scripts.js
+++ scripts.js
@@ -7,3 +7,6 @@ document.querySelectorAll('.infocard-list-pkmn-lg')[0].style.background = 'azure
 
 const webUrl = document.head.childNodes[9].href
 console.log(webUrl)
+
+console.log(webUrl.split('/')[2])
+
