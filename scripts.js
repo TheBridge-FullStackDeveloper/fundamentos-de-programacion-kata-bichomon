12cea22e4c22bc6662caff00e2fcf8ba2b1f6c06
Brujitari <natimontesg2@mail.com>
2022-02-20 21:37:11 +0100

    feat: second exercise refactored

--- scripts.js
+++ scripts.js
@@ -5,4 +5,5 @@ document.getElementById('gen-1').innerText = 'Generasión 1 Pokimon'
 
 document.querySelectorAll('.infocard-list-pkmn-lg')[0].style.background = 'azure'
 
-console.log(document.head.childNodes[9].href)
+const webUrl = document.head.childNodes[9].href
+console.log(webUrl)
