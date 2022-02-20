6b09f215a176b526d008a1e1234eec2da9b927ab
Brujitari <natimontesg2@mail.com>
2022-02-20 21:30:21 +0100

    feat: console displays URL

--- scripts.js
+++ scripts.js
@@ -6,3 +6,8 @@ title = 'Generasión 1 Pokimon'
 
 document.querySelectorAll('.infocard-list-pkmn-lg')[0].style.background = 'azure'
 
+console.log(document.head.childNodes[9].href)
+
+
+
+/* <meta property="og:url" content="https://pokemondb.net/pokedex/national"> */
